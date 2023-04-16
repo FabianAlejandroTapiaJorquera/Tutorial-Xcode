ad2b786d221a532912255d28273fda8c24b6b9b6
Fabian Alejandro Tapia Jorquera <fabian_alejandro_tapia@hotmail.com>
2023-04-16 18:05:36 -0400

    Initial Commit

---
+++ Landmarks/Preview Content/Preview Assets.xcassets/Landmarks/Landmarks 1/LandmarksApp.dataset/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Fabian Tapia Jorquera on 09-04-23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,29 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Fabian Tapia Jorquera on 16-04-23.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView {
+            List(landmarks) { landmark in
+                NavigationLink {
+                    LandmarkDetail()
+                } label: {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
